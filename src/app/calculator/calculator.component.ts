08e315818c89091a8e61ff1a847fbcc506f14f90
Lucas Silva <victorlucassls@gmail.com>
2020-04-07 10:31:34 -0300

    Criando primeiras funções

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatButtonModule} from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-calculator',
@@ -13,4 +13,42 @@ export class CalculatorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  input1
+  input2
+  
+  brl:number = 1
+  onDollar(event) {
+    let dollar = 5.20
+    this.input1 = 1
+    this.input2 = dollar
+  }
+  onEuro(event) {
+    let euro = 5.68
+    this.input1 = 1
+    this.input2 = euro
+  }
+  onPound(event) {
+    let pound = 5.75
+    this.input1 = 1
+    this.input2 = pound
+  }
+  onCanadianDollar(event) {
+    let canadianDollar = 3.72
+    this.input1 = 1
+    this.input2 = canadianDollar
+  }
+  onAustralianDollar(event) {
+    let australianDollar = 3.23
+    this.input1 = 1
+    this.input2 = australianDollar
+  }
+
+
+  onChangeCountry(event) {
+
+  }
+  onChangeCoin(event) {
+
+  }
 }
