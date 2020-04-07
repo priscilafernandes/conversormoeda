4afdfa60571f2dcfc429f9b625bcf98718890b00
Lucas Silva <victorlucassls@gmail.com>
2020-04-07 11:12:45 -0300

    organizando as funções

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -13,42 +13,48 @@ export class CalculatorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  euro = 5.68
+  dollar = 5.20
+  pound = 5.75
+  australianDollar = 3.23
+  canadianDollar = 3.72
+
 
   input1
   input2
-  
-  brl:number = 1
+
+  brl: number = 1
+
   onDollar(event) {
-    let dollar = 5.20
     this.input1 = 1
-    this.input2 = dollar
+    this.input2 = this.dollar
   }
   onEuro(event) {
-    let euro = 5.68
     this.input1 = 1
-    this.input2 = euro
+    this.input2 = this.euro
   }
   onPound(event) {
-    let pound = 5.75
     this.input1 = 1
-    this.input2 = pound
+    this.input2 = this.pound
   }
   onCanadianDollar(event) {
-    let canadianDollar = 3.72
     this.input1 = 1
-    this.input2 = canadianDollar
+    this.input2 = this.canadianDollar
   }
   onAustralianDollar(event) {
-    let australianDollar = 3.23
     this.input1 = 1
-    this.input2 = australianDollar
+    this.input2 = this.australianDollar
   }
 
 
-  onChangeCountry(event) {
-
+  onChangeInput1(event) {
+    this.input1 = event.target.value
+    let divider = this.input1 * this.input2
+    this.input2 = divider.toFixed(3)
   }
-  onChangeCoin(event) {
-
+  onChangeInput2(event) {
+  this.input2 = event.target.value
+  let multiply = this.input2 / this.input1
+  this.input1 = multiply.toFixed(3)
   }
 }
