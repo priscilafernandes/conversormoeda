bd0dedd6f109adc2fbb9b46f0472876a14d8ec5f
priscilafernandes <pris1@outlook.com.br>
2020-04-08 20:46:21 -0300

    conversao das moedas com valores da api funcionando

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,83 +7,88 @@ import { CurrencyConversionService } from '../currency-conversion.service'
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
-
-  constructor(private currency: CurrencyConversionService) { }
-  //Valor Inicial setado //
-  ngOnInit(): void {
-    this.intercoin = this.coinDefaultValue
-    this.brcoin = this.dollar.toFixed(2)
-    this.CountryChoose = 'Dólar Americano'
-    this.SignSpan = 'US$'
-   
-  }
   // Variáveis que armazenam dados do Input //
   intercoin
   brcoin
-
   formatter: any
   CountryChoose = ''
   SignSpan = ''
   SignFix = 'R$'
   coinDefaultValue = (1.00).toFixed(2)
-  euro = 5.68
-  dollar = 5.20
-  pound = 5.75
-  australianDollar = 3.23
-  canadianDollar = 3.72
+
+  // moedas
+  dollar: number = 0
+  australianDollar: number = 0
+  canadianDollar: number = 0
+  euro: number = 0
+  pound: number = 0
+
+  constructor(private currency: CurrencyConversionService) { }
+
+  //Valor Inicial setado //
+  ngOnInit(): void {
+    this.getCoin("USD")
+    this.CountryChoose = 'Dólar Americano'
+    this.SignSpan = 'US$'
+    this.intercoin = this.coinDefaultValue
+    this.brcoin = this.dollar.toFixed(2)
+  }
 
   //Função Api //
+  getCoin(base: string) {
+    this.currency.getCurrency(base).subscribe((data) => {
+      let coin = new Object(data);
 
-  
-  getCoin(base, money){
-    let coin;
-    this.currency.getCurrency(base).subscribe(
-      (data) => {
-        coin = new Object(data)
-        //console.log(coin.rates)
-        return coin.rates[money]
-      });
-    }
+      this.dollar = coin.rates.BRL
+      this.australianDollar = coin.rates.BRL
+      this.canadianDollar = coin.rates.BRL
+      this.euro = coin.rates.BRL
+      this.pound = coin.rates.BRL
+      this.brcoin = coin.rates.BRL.toFixed(2)
+
+      console.log(coin)
+    })
+  }
+
   // Funções dos botões //
   onDollar(event) {
     this.CountryChoose = 'Dólar Americano'
     this.SignSpan = 'US$'
     this.intercoin = this.coinDefaultValue
-<<<<<<< HEAD
     this.brcoin = this.dollar.toFixed(2)
-=======
-    this.brcoin = this.dollar.toFixed(2)  
-    console.log(this.getCoin('USD', 'USD'));
-    this.getCoin('USD', 'USD')
->>>>>>> 4d4247f36e0b17f8e2f3a6d7a6dca60f3cfd126d
+    this.getCoin("USD")
   }
-  onEuro(event) {
-    this.CountryChoose = 'Euro'
-    this.SignSpan = '€'
-    this.intercoin = this.coinDefaultValue
-    this.brcoin = this.euro.toFixed(2)
-    // this.getCoin('EUR')
-  }
-  onPound(event) {
-    this.CountryChoose = 'Libra'
-    this.SignSpan = '£'
-    this.intercoin = this.coinDefaultValue
-    this.brcoin = this.pound.toFixed(2)
-    // this.getCoin('GBP')
-  }
-  onCanadianDollar(event) {
-    this.CountryChoose = 'Dólar Canadense'
-    this.SignSpan = 'CA$'
-    this.intercoin = this.coinDefaultValue
-    this.brcoin = this.canadianDollar.toFixed(2)
-    // this.getCoin('CAD')
-  }
+
   onAustralianDollar(event) {
     this.CountryChoose = 'Dólar Australiano'
     this.SignSpan = 'AU$'
     this.intercoin = this.coinDefaultValue
     this.brcoin = this.australianDollar.toFixed(2)
-    // this.getCoin('AUD')
+    this.getCoin('AUD')
+  }
+
+  onCanadianDollar(event) {
+    this.CountryChoose = 'Dólar Canadense'
+    this.SignSpan = 'CA$'
+    this.intercoin = this.coinDefaultValue
+    this.brcoin = this.canadianDollar.toFixed(2)
+    this.getCoin('CAD')
+  }
+
+  onEuro(event) {
+    this.CountryChoose = 'Euro'
+    this.SignSpan = '€'
+    this.intercoin = this.coinDefaultValue
+    this.brcoin = this.euro.toFixed(2)
+    this.getCoin('EUR')
+  }
+
+  onPound(event) {
+    this.CountryChoose = 'Libra'
+    this.SignSpan = '£'
+    this.intercoin = this.coinDefaultValue
+    this.brcoin = this.pound.toFixed(2)
+    this.getCoin('GBP')
   }
 
   // Switch case  para cada opção de moeda //
