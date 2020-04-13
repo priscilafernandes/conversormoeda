840617c34459a32ae5f063377950b7faa3cda152
Mayke <mayke.sp11@hotmail.com>
2020-04-13 10:04:08 -0300

    atualização de código

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http'
+import { CurrencyConversionService } from './currency-conversion.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculatorComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatInputModule,
+    HttpClientModule,
+    
+  ],
+  providers: [CurrencyConversionService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
